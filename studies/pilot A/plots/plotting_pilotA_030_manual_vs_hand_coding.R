801ce54fd0b05cfe7cef9e1c6a841c57b8b0b64f
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2020-06-23 20:22:39 +0200

    update plotting

---
+++ studies/pilot A/plots/plotting_pilotA_030_manual_vs_hand_coding.R
@@ -0,0 +1,47 @@
+
+### PLOTTING PILOT A - 030 - manual coding vs. hand coding
+
+# Gregor KAchel, Mandy Klatt
+# January, 2020
+
+##########################################################################
+### load Libraries / install packages
+##########################################################################
+
+## http://www.eyetracking-r.com/
+
+
+# install.packages("tidyverse")
+# install.packages("lemon")
+
+library(tidyverse) # contains ggplot2, dplyr...and all the other good things
+library(lemon) # necessary for arranging multiple plots next to each other
+
+### MANUAL CODING
+##########################################################################
+### Prepare data event durations and proportion durations looking at Gregor, plant, teddy and computer
+##########################################################################
+
+# read in data while specifying locale allows to set "," 
+data_raw<-read_tsv("./Mandy1_PilotA_data_export_hand_coding_events.tsv", 
+                   locale = locale(decimal_mark = ","))
+
+view(data_raw)
+
+
+# select relevant columns via SELECT
+event_duration <- data_raw  %>% select(
+  'Recording timestamp',
+  Event, 
+  )
+
+ view(event_duration)
+ 
+ 
+# summarize time spend looking at Gregor
+ event_duration_sum <- event_duration %>%
+   group_by(Event) %>%
+   summarize(gregor = sum(B),
+             sumBfoo = sum(B[A=="foo"]))
+ 
+ 
---
+++ studies/pilot A/plots/plotting_pilotA_031_material_vs_class.R
@@ -0,0 +1,57 @@
+### PLOTTING PILOT A - 031 - material vs. class - automatic mapping
+
+# Gregor KAchel, Mandy Klatt
+# July, 2020
+
+##########################################################################
+### load Libraries / install packages
+##########################################################################
+
+## http://www.eyetracking-r.com/
+
+
+install.packages("tidyverse")
+install.packages("lemon")
+
+library(tidyverse) # contains ggplot2, dplyr...and all the other good things
+library(lemon) # necessary for arranging multiple plots next to each other
+
+### AUTOMATIC MAPPING
+##########################################################################
+### Prepare data TOTAL DURATION and PROPORTION DURATION of looking at AOIs Gregor, plant, teddy and computer
+##########################################################################
+
+# read in data while specifying locale allows to set "," 
+data_raw<-read_tsv("./Mandy1_PilotA_031_metrics_class.tsv", 
+                   locale = locale(decimal_mark = ","))
+
+view(data_raw)
+
+### TOTAL DURATION
+
+# select relevant columns via SELECT
+class_data_NA <- data_raw  %>% select(
+  Total_duration_of_fixations.computer,
+  Total_duration_of_fixations.gregor,
+  Total_duration_of_fixations.plant,
+  Total_duration_of_fixations.teddy
+  )
+
+view(class_data_NA)
+
+# drop rows with NAs
+class_data <- class_data_NA %>% filter(!is.na(Total_duration_of_fixations.computer+
+                                              Total_duration_of_fixations.gregor+
+                                              Total_duration_of_fixations.plant+
+                                              Total_duration_of_fixations.teddy))
+
+view(class_data)
+
+# create new column names that give total duration of looking A, B, Teddy and Urmenschen
+total_duration <- class_data  %>% 
+  rename (Computer = Total_duration_of_fixations.computer,
+          Gregor = Total_duration_of_fixations.gregor,
+          Plant = Total_duration_of_fixations.plant,
+          Teddy = Total_duration_of_fixations.teddy)
+
+view(total_duration)
