795b95dd9971cb5f215b2fb67bc381b693c984ee
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2021-03-05 14:39:07 +0100

    Update AOI_plots.R

--- paper_1/AOI_plots.R
+++ paper_1/AOI_plots.R
@@ -12,12 +12,28 @@ options(dplyr.summarise.inform = FALSE)
 # prepare data (selected from AOI data)
 et.raw.data<-read.table("./data/Aperol_pilot_glasses_AOI.txt", dec=",", sep="\t", header=T) 
 
+# convert into numeric
+et.raw.data <- et.raw.data %>%  mutate(Total.duration.of.fixation.in.AOI..sec. = as.numeric(Total.duration.of.fixation.in.AOI..sec.),
+                                       Average.duration.of.fixation.in.AOI..sec. = as.numeric(Average.duration.of.fixation.in.AOI..sec.),
+                                       Number.of.fixations.in.AOI = as.numeric (Number.of.fixations.in.AOI),
+                                       Total.Time.of.Interest.Duration..sec. = as.numeric(Total.Time.of.Interest.Duration..sec.))
+
+
 # create a basic table (tibble) using tidyverse functions for all disruptions 
 # Change Number of Digits in Global R Options to reduce the number of digits after the decimal point to two
 options(digits = 2)              
 
-et.table <- et.raw.data %>% # select data
+et.table <- et.raw.data %>%
   group_by(Type.of.disruption, Group) %>%
-  summarise("Total Duration of Fixations" = Total.duration.of.fixation.in.AOI..sec.,
-            "Average Duration of Fixations" = Average.duration.of.fixation.in.AOI..sec.,
-            "Number of Fixations" = Number.of.fixations.in.AOI)
+  filter(Type.of.disruption != "staring out the window") %>%
+  rename ('Type of Disruption' = Type.of.disruption) %>%
+  summarise("Mean Duration of Fixations" = mean(Total.duration.of.fixation.in.AOI..sec.),
+            "Number of Fixations" = mean(Number.of.fixations.in.AOI),
+            "TOI" = mean(Total.Time.of.Interest.Duration..sec.))
+ 
+# create new columns that give proportion of the duration of looking at AOIs
+# relative duration divided by total duration is the proportion
+proportion.et.data <- et.raw.data %>% 
+  group_by(Type.of.disruption, Group) %>%
+  summarise(Proportion = Average.duration.of.fixation.in.AOI..sec./Total.duration.of.fixation.in.AOI..sec.)
+
