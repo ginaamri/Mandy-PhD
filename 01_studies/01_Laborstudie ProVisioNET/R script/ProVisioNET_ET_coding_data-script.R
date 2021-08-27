fbc67d769e5c6588bf00983bc9c85fd93cbaec2b
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2021-08-27 14:57:48 +0200

    Update ProVisioNET_ET_coding_data-script.R

--- 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNET_ET_coding_data-script.R
+++ 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNET_ET_coding_data-script.R
@@ -16,12 +16,19 @@ options(dplyr.summarise.inform = FALSE)
 
 r_refs("r-references.bib")
 
+
+################## RATER 1 ################
+
 # read in data from rater1 while specifying locale allows to set "," 
 coding_data_r1 <-read_tsv ("./data/03_03_EH_ProVisioNET_study_glasses Metrics_event-based.tsv", 
                    locale = locale(decimal_mark = ","))
 
+# filter only rows lesson
+r1 <- coding_data_r1 %>% filter(TOI == "Lesson")
+
 # select variable AOI + Hit_proportion
-r1 <- coding_data_r1 %>% select(AOI, Hit_proportion)
+r1 <- r1 %>% select(AOI, Hit_proportion)
+
 
 #delete NAs
 r1 <- na.omit(r1)
@@ -34,14 +41,60 @@ r1 <- aggregate(r1,
                 by = list(r1$AOI),
                 FUN = length)
 
+# select only one row and rename columns 
+r1 <- r1 %>% select (Group.1, Hit_proportion) %>%
+              rename(AOI = Group.1,
+                     AOI_hit_r1 = Hit_proportion)  
+    
 r1
 
+################## RATER 2 ################
+
+# read in data from rater1 while specifying locale allows to set "," 
+coding_data_r2 <-read_tsv ("./data/03_03_RD_ProVisioNET_study_glasses Metrics_event-based.tsv", 
+                           locale = locale(decimal_mark = ","))
+
+# filter only rows lesson
+r2 <- coding_data_r2 %>% filter(TOI == "Lesson")
+
+# select variable AOI + Hit_proportion
+r2 <- r2 %>% select(AOI, Hit_proportion)
 
 
+#delete NAs
+r2 <- na.omit(r2)
+
+# round Hit_proportion to 1
+options(digits = 0)              
+
+#aggregate data by AOI
+r2 <- aggregate(r2,
+                by = list(r2$AOI),
+                FUN = length)
+
+# select only one row and rename columns 
+r2 <- r2 %>% select (Group.1, Hit_proportion) %>%
+  rename(AOI = Group.1,
+         AOI_hit_r2 = Hit_proportion)  
+
+r2
 
 
+###### filter only for identical rows for both rater
 
+r1 <- r1 %>% filter(AOI != "Board_Screen",
+                    AOI != "Disruptive_Person,students_material", 
+                    AOI != "Material",
+                    AOI != "students_material")
 
+r1
+
+r2 <- r2 %>% filter(AOI != "Board_Screen",
+                    AOI != "Anna,Bianca,Disruptive_Person",
+                    AOI != "Disruptive_Person,students_material", 
+                    AOI != "Material",
+                    AOI != "students_material")
+r2
 
 
 
