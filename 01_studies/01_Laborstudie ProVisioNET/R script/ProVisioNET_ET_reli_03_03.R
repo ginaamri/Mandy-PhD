2df6a22819d205907aeb38f5eda252e30efeb132
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2021-09-13 18:04:36 +0200

    R script TOI AOI

--- 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNET_ET_reli_03_03.R
+++ 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNET_ET_reli_03_03.R
@@ -1,16 +1,17 @@
 ### ProVisioNET pilot data 
-#### intercoder reli eye tracking data 01_01
+#### intercoder reli eye tracking data 03_03
 
 
 # if a package is not installed on the current machine, it will install it
-library(needs)
-needs(tidyverse,
-      psych,
-      moments,
-      sjPlot,
-      DescTools,
-      irr,
-      readxl)
+if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
+if (!require(tidyverse)) install.packages('tidyverse'); library(tidyverse)
+if (!require(papaja)) install.packages("papaja"); library(papaja)
+if (!require(psych)) install.packages('psych'); library(psych) # stats
+if (!require(moments)) install.packages('moments'); library(moments) # skewness & kurtosis
+if (!require(sjPlot)) install.packages('sjPlot'); library(sjPlot) # item analysis of a scale or index
+if (!require(DescTools)) install.packages('DescTools'); library(DescTools) # cohens kappa
+if (!require(irr)) install.packages('irr'); library(irr) # Various Coefficients of Interrater Reliability and Agreement
+
 
 # suppress "summarize" info. 
 # if this line is ommitted, each table using the summarize function will be accompanied with a warning from the console
@@ -22,8 +23,8 @@ r_refs("r-references.bib")
 ################## RATER 1 ################
 
 # read in data from rater1 while specifying locale allows to set "," 
-coding_data_r1 <-read_tsv ("./data/01_01_AP_ProVisioNET_study_glasses_Metrics_Event-based.tsv", 
-                           locale = locale(decimal_mark = ","))
+coding_data_r1 <-read_tsv ("./data/03_03_EH_ProVisioNET_study_glasses Metrics_event-based.tsv", 
+                   locale = locale(decimal_mark = ","))
 
 # filter only rows lesson
 r1 <- coding_data_r1 %>% filter(TOI == "Lesson")
@@ -46,7 +47,7 @@ r1_aggr <- group_by(.data = r1,
 ################## RATER 2 ################
 
 # read in data from rater1 while specifying locale allows to set "," 
-coding_data_r2 <-read_tsv ("./data/01_01_MK_ProVisioNET_study_glasses_Metrics_Event-based.tsv", 
+coding_data_r2 <-read_tsv ("./data/03_03_RD_ProVisioNET_study_glasses Metrics_event-based.tsv", 
                            locale = locale(decimal_mark = ","))
 
 # filter only rows lesson
@@ -68,25 +69,27 @@ r2_aggr <- group_by(.data = r2,
                     AOI) %>% summarise(Hit_count = length(Hit_proportion))
 
 
-# ###### filter only for identical rows for both rater
-# 
-# r1_filter <- r1_aggr %>% filter(AOI != "Board_Screen",
-#                                 AOI != "Disruptive_Person,students_material", 
-#                                 AOI != "Material",
-#                                 AOI != "students_material")
-# 
-# r1_filter
-# 
-# r2_filter <- r2_aggr %>% filter(AOI != "Board_Screen",
-#                                 AOI != "Anna,Bianca,Disruptive_Person",
-#                                 AOI != "Disruptive_Person,students_material", 
-#                                 AOI != "Material",
-#                                 AOI != "students_material")
-# r2_filter
+
+###### filter only for identical rows for both rater
+
+r1_filter <- r1_aggr %>% filter(AOI != "Board_Screen",
+                    AOI != "Disruptive_Person,students_material", 
+                    AOI != "Material",
+                    AOI != "students_material")
+
+r1_filter
+
+r2_filter <- r2_aggr %>% filter(AOI != "Board_Screen",
+                    AOI != "Anna,Bianca,Disruptive_Person",
+                    AOI != "Disruptive_Person,students_material", 
+                    AOI != "Material",
+                    AOI != "students_material")
+r2_filter
 
 # merge two data frames --> adding Columns, don't forget to merge BY AOI !
 
-r3 <- merge(r1_aggr, r2_aggr, by = 'AOI')
+r3 <- merge(r1_filter, r2_filter, by = 'AOI')
+
 
 #################### Percentage Agreement ##############################
 
@@ -113,3 +116,5 @@ data_icc <- subset(r3, select = c(Hit_count.x, Hit_count.y))
 
 # calculate the ICC for two raters with new subset
 ICC(data_icc)
+
+
