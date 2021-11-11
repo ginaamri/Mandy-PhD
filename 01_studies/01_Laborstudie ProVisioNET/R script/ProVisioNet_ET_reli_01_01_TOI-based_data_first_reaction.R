0d0cb6394771899b97b6104ccd3afcffd528eccd
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2021-11-11 16:20:07 +0100

    update R script

--- 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNet_ET_reli_01_01_TOI-based_data_first_reaction.R
+++ 01_studies/01_Laborstudie ProVisioNET/R script/ProVisioNet_ET_reli_01_01_TOI-based_data_first_reaction.R
@@ -135,20 +135,61 @@ df1 <- subset.data.frame(reaction_ratings, select = c(Time_to_first_Reaction.x))
 df2 <- subset.data.frame(reaction_ratings, select = c(Time_to_first_Reaction.y))
 
 # add the difference as a new column
-reaction_ratings$Diff <- df1 - df2
+reaction_ratings$diff <- df1 - df2
 
 # creating only absolute values with abs()
-reaction_ratings$Diff <- abs(reaction_ratings$diff) 
+reaction_ratings$diff <- abs(reaction_ratings$diff) 
 
 # if rating difference > 2 seconds --> 0
-reaction_ratings$Diff = ifelse(reaction_ratings$"Diff"<2, "1","0")
+reaction_ratings$diff = ifelse(reaction_ratings$"diff"<2, "1","0")
+reaction_ratings$diff <- as.numeric(reaction_ratings$diff)
 
+# add a new column with values --> 0 / 1 
+reaction_ratings$r2 = ifelse(reaction_ratings$"diff">2, "0","1")
+
+
+### GREGOR
+# load packages
+if (!require(irr)) install.packages('irr'); library(irr) # cohens kappa
+if (!require(irrCAC)) install.packages('irrCAC'); library(irrCAC) # cohens kappa
 
 # calculating Cohen.Kappa
-reaction_ratings_kappa <- select(reaction_ratings, Diff)
+psych::cohen.kappa(x = as.matrix(reaction_ratings_kappa))
+reaction_ratings_kappa <- select(reaction_ratings, diff, r2)
+reaction_ratings_kappa
+reli.reaction_ratings.res <- kappa2(reaction_ratings_kappa, "unweighted")
+reli.reaction_ratings.res
 
-reaction_ratings_kappa$Diff <- as.numeric(reaction_ratings_kappa$Diff)
+###################################################################
+
+
+# create percent agreement for validity
+reli.reaction_ratings.data <- reli.reaction_ratings.data  %>%
+  mutate(countagreement = if_else(diff == r2, 1, 0) )
+
+
+# create percent agreement for validity
+reli.reaction_ratings.data <- reli.reaction_ratings.data  %>%
+  mutate(countagreement = if_else(diff == r2, 1, 0) )
+
+reli.reaction_ratings.agreement <- round(mean(reli.reaction_ratings.agreement.data$countagreement),2)*100
+reli.reaction_ratings.agreement
+
+
+reli.reaction_ratings.data
+
+
+
+
+
+
+reaction_ratings_kappa <- select(reaction_ratings,
+                      diff, r2) 
+
+reaction_ratings_kappa <- as_factor(reaction_ratings_kappa)
 
 psych::cohen.kappa(x = as.matrix(reaction_ratings_kappa))
+
+
 
 
