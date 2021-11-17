53639a8132a5bc8b7924668df265a337de19515d
MandyKlatt <56037386+MandyKlatt@users.noreply.github.com>
2021-11-17 15:03:55 +0100

    SRI plot

--- 01_studies/01_Laborstudie ProVisioNET/R script/sri coding/sri coding.R
+++ 01_studies/01_Laborstudie ProVisioNET/R script/sri coding/sri coding.R
@@ -8,7 +8,8 @@ needs(tidyverse,
       sjPlot,
       DescTools,
       irr,
-      readxl)
+      readxl, 
+      gridExtra)
 
 # suppress "summarize" info. 
 # if this line is ommitted, each table using the summarize function will be accompanied with a warning from the console
@@ -21,56 +22,72 @@ sri_expert <- read_excel("./data/coding_sri_expert.xlsx")
 
 
 # filter relevant rows and select relevant columns 
-sri_novice <- sri_novice %>% select(group, event, disturbing_factor, safety_factor)
+sri_novice <- sri_novice %>% select(Group, Event, Disturbing_Factor, Confident_Factor)
 
-sri_expert <- sri_expert %>% select(group, event, disturbing_factor, safety_factor)
+sri_expert <- sri_expert %>% select(Group, Event, Disturbing_Factor, Confident_Factor)
 
 
-# combine the two data frames
+# combine the two data frames with novice and expert
 sri <- rbind(sri_expert, sri_novice)
 
-# create a new data frame
-sri_disturb <- subset.data.frame(sri, select = c(group, event, disturbing_factor))
 
-sri_safe <- subset.data.frame(sri, select = c(group, event, safety_factor))
+# removing all NAs
+sri_filter <- na.omit(sri)
+
+# # replacing NA with 11
+# sri[is.na(sri)] = 11
+
+# # creating a new column --> event was "seen" / "not seen" 
+# # WARNING! ifelse() needs three parameters: test, truth value, false value 
+# sri$Seen <- ifelse(sri$Disturbing_Factor <= 10, 'seen', ifelse(sri$Disturbing_Factor > 11, 'not seen', 'not seen')) 
+
 
 # define expert and novice with ifelse function
-sri_disturb$group = ifelse(sri_disturb$group < 200, "Novice","Expert")
+sri_filter$Group = ifelse(sri_filter$Group < 200, "Novice","Expert")
 
-sri_safe$group = ifelse(sri_safe$group < 200, "Novice","Expert")
+# create a new data frame with both factors
+sri_disturb <- subset.data.frame(sri_filter, select = c(Group, Event, Disturbing_Factor))
+sri_confi <- subset.data.frame(sri_filter, select = c(Group, Event, Confident_Factor))
 
 
 # plotting disturbing factor
 dist_plot <- 
   ggplot(data = sri_disturb,
-         mapping = aes(x = group,
-                       y = "safety_factor")) +
-  geom_boxplot(mapping = aes(fill = group)) +
+         mapping = aes(x = Group,
+                       y = Disturbing_Factor)) +
+  geom_boxplot(mapping = aes(fill = Group)) +
   geom_point(size = 2, 
              alpha = 0.4,
              position = position_jitter(seed = 1, 
                                         width = 0.1)) +
+  ylim(0,10)+
   scale_fill_brewer(palette = "RdBu") +
-  facet_wrap(vars(event), 
-             nrow = 1) +
-  theme_blank()
-
+  facet_wrap(vars(Event), 
+             nrow = 1, strip.position = "bottom") +
+  ggtitle("How disturbing was the event for you?") +
+  theme(axis.ticks.x = element_blank(),
+        axis.text.x = element_blank())
 dist_plot
 
 
-# plotting safety factor
-safe_plot <- 
-  ggplot(data = sri_long,
-         mapping = aes(x = event,
-                       y = "Value")) +
-  geom_boxplot(mapping = aes(fill = group)) +
+# plotting Confident factor
+confi_plot <- 
+  ggplot(data = sri_confi,
+         mapping = aes(x = Group,
+                       y = Confident_Factor)) +
+  geom_boxplot(mapping = aes(fill = Group)) +
   geom_point(size = 2, 
              alpha = 0.4,
              position = position_jitter(seed = 1, 
                                         width = 0.1)) +
   scale_fill_brewer(palette = "RdBu") +
-  facet_wrap(vars(event), 
-             nrow = 1) +
-  theme_blank()
+  facet_wrap(vars(Event), 
+             nrow = 1, strip.position = "bottom") +
+  ggtitle("How confident did you feel in dealing with this event?") +
+  theme(axis.ticks.x = element_blank(),
+        axis.text.x = element_blank())
 
-safe_plot
+confi_plot
+
+# arraning plots 
+grid.arrange(dist_plot, confi_plot, ncol=2, nrow =1)
